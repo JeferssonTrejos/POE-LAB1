075d8a0bc3713b8f7fbae05190db7b230764cd62
Aristides <76984300+Stevan7722@users.noreply.github.com>
2024-08-13 21:08:55 -0600

    Add files via upload

---
+++ ejercicio4App/ejercicio4/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox = new TextBox();
+            label1 = new Label();
+            textLabel = new Label();
+            SuspendLayout();
+            // 
+            // textBox
+            // 
+            textBox.Location = new Point(183, 148);
+            textBox.Name = "textBox";
+            textBox.Size = new Size(481, 27);
+            textBox.TabIndex = 1;
+            textBox.TextChanged += textBox_TextChanged;
+            textBox.KeyPress += textBox_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(362, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 31);
+            label1.TabIndex = 2;
+            label1.Text = "Ejercicio 4";
+            // 
+            // textLabel
+            // 
+            textLabel.AutoSize = true;
+            textLabel.Location = new Point(185, 196);
+            textLabel.Name = "textLabel";
+            textLabel.Size = new Size(0, 20);
+            textLabel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textLabel);
+            Controls.Add(label1);
+            Controls.Add(textBox);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox;
+        private Label label1;
+        private Label textLabel;
+    }
+}
---
+++ ejercicio4App/ejercicio4/Form1.cs
@@ -0,0 +1,28 @@
+namespace ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Space)
+            {
+                textLabel.Text = "cuidado! No se permite presionar la Barra Espaciadora";
+                e.Handled = true;
+            }
+            else
+            {
+                textLabel.Text = "";
+            }
+        }
+    }
+}
