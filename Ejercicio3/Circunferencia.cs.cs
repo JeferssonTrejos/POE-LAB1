e0f85d661963c7a47546817af256f80e721029d4
AlejandroVasquez503 <140095864+AlejandroVasquez503@users.noreply.github.com>
2024-08-14 20:10:47 -0600

    Circunferencia.cs.cs

--- Ejercicio3/Circunferencia.cs.cs
+++ Ejercicio3/Circunferencia.cs.cs
@@ -3,19 +3,19 @@
 namespace FigurasGeometricas
 {
     public class Circunferencia
-    {
+    {    // Almacena el valor del radio de la circunferencia
         public double Radio { get; set; }
-
+        // Inicializa la circunferencia con un valor de radio
         public Circunferencia(double radio)
         {
             Radio = radio;
         }
-
+        // Calcula el área de la circunferencia
         public double CalcularArea()
         {
             return Math.PI * Radio * Radio;
         }
-
+        // Calcula el perímetro de la circunferencia
         public double CalcularPerimetro()
         {
             return 2 * Math.PI * Radio;
