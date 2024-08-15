2a6ebeb77559e880884597e7b8d9de8532ee0002
AlejandroVasquez503 <140095864+AlejandroVasquez503@users.noreply.github.com>
2024-08-14 20:09:42 -0600

    Update Cuadrado.cs

--- Ejercicio3/Cuadrado.cs
+++ Ejercicio3/Cuadrado.cs
@@ -3,19 +3,19 @@
 namespace FigurasGeometricas
 {
     public class Cuadrado
-    {
+    {    // Almacena el valor del lado del cuadrado
         public double Lado { get; set; }
-
+        // Inicializa el cuadrado con un valor de lado
         public Cuadrado(double lado)
         {
             Lado = lado;
         }
-
+         // Calcula el área del cuadrado
         public double CalcularArea()
         {
             return Lado * Lado;
         }
-
+        // Calcula el perímetro del cuadrado
         public double CalcularPerimetro()
         {
             return 4 * Lado;
