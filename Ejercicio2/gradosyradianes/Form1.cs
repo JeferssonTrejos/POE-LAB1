eafdafc502a2b63c61b06e382e82d80bb831b66e
AlejandroVasquez503 <140095864+AlejandroVasquez503@users.noreply.github.com>
2024-08-14 20:06:34 -0600

    Update Form1.cs

--- Ejercicio2/gradosyradianes/Form1.cs
+++ Ejercicio2/gradosyradianes/Form1.cs
@@ -6,33 +6,34 @@ namespace gradosyradianes
         {
             InitializeComponent();
         }
-
+        // Evento para calcular el coseno en grados cuando se hace clic en el bot√≥n "Grados"
         private void GradosButton_MouseClick(object sender, MouseEventArgs e)
-        {
+        {    // Verifica que el valor sea valido
             if (double.TryParse(ValorTerabox.Text, out double anguloGrados))
-            {
+            {    // Convierte los grados a radianes para el c√°lculo del coseno
                 double anguloRadianes = anguloGrados * Math.PI / 180;
                 double coseno = Math.Cos(anguloRadianes);
                 coseno = Math.Round(coseno, 2);
+                // Calcula el coseno y lo muestra en el cuadro de resultado con 4 decimales
                 ResultadoLabel.Text = $"Coseno en grados: {coseno}";
             }
             else
             {
-                MessageBox.Show("Por favor ingresa un valor numÈrico v·lido.");
+                MessageBox.Show("Por favor ingresa un valor num√©rico v√°lido.");
             }
         }
-
+        // Evento para calcular el coseno en radianes cuando se hace clic en el bot√≥n "Radianes"
         private void RadianesButton_MouseClick(object sender, MouseEventArgs e)
-        {
+        {    // Verifica que el valor ingresado sea valido
             if (double.TryParse(ValorTerabox.Text, out double anguloRadianes))
-            {
+            {    // Calcula el coseno directamente en radianes
                 double coseno = Math.Cos(anguloRadianes);
                 coseno = Math.Round(coseno, 2);
                 ResultadoLabel.Text = $"Coseno en radianes: {coseno}";
             }
             else
             {
-                MessageBox.Show("Por favor ingresa un valor numÈrico v·lido.");
+                MessageBox.Show("Por favor ingresa un valor num√©rico v√°lido.");
             }
         }
 
